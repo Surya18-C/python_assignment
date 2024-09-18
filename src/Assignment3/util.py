1170d410bfc9ca2b2ddf3edeba511a9036edf723
Surya <surya.c@diggibyte.com>
2024-09-18 16:55:28 +0530

    Convert immutable string to mutable

---
+++ src/Assignment3/util.py
@@ -0,0 +1,16 @@
+
+def mutation():
+    str_val = "Apple"
+
+    # Convert the string to a list of characters
+    lis = list(str_val)
+
+    # Modify the specific character in the list
+    lis[4] = 'A'
+
+    # Join the list back into a string
+    new_str = ''.join(lis)
+
+    # Print the modified string
+    print(new_str)
+
