a7c09430fb0f0b85218a417a1236f34ceb8a343b
Surya <surya.c@diggibyte.com>
2024-09-18 15:12:21 +0530

    Finding Average in students mark

---
+++ src/Assignment1/util.py
@@ -0,0 +1,20 @@
+
+def average():
+    student = {
+        "babu": [20, 30, 40],
+        "Anu": [50, 30, 40],
+        "rex": [45, 35, 30]
+    }
+
+    student_name=input("Enter Student Name : ")
+    mark=student[student_name]
+
+    total=0
+    for i in mark:
+        total+=i
+
+    avg = total/len(student)
+
+    print("Average : ",avg)
+
+
