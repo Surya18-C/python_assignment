7ecea59c3383d01277bf6443f0a3c8ec07d0bed7
Surya <surya.c@diggibyte.com>
2024-09-22 11:34:03 +0530

    Assignment 5

---
+++ src/Assignment5/driver.py
@@ -0,0 +1,5 @@
+
+import util
+
+for lis in util.str_format(5):
+    print(lis)
---
+++ src/Assignment5/util.py
@@ -0,0 +1,8 @@
+def str_format(n):
+    res = []
+    for i in range(1, n + 1):
+        format_str = f"{i} {oct(i)[2:]} {hex(i)[2:]} {bin(i)[2:]}"
+        res.append(format_str)
+    return res
+
+
