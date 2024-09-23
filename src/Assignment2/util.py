3793b1456e68c7cb18efeeb17223568b8aad672f
Surya <surya.c@diggibyte.com>
2024-09-23 17:52:13 +0530

    Find the Runner-Up Score

--- src/Assignment2/util.py
+++ src/Assignment2/util.py
@@ -1,7 +1,6 @@
 
 """ Given the participants' score sheet for your University Sports Day, you are required
-to find the runner-up score. You are given  scores. Store them in a list
-and find the score of the runner-up."""
+to find the runner-up score."""
 
 def score(scores):
     first=scores[0]
