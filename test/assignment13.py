3190ff8cb9a62f98fb6e40c08b538d08994781ea
Surya <surya.c@diggibyte.com>
2024-09-23 16:42:25 +0530

    Assignment13 test file

--- test/assignment13.py
+++ test/assignment13.py
@@ -4,7 +4,7 @@ from ..src.Assignment13.utill import min_max
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(min_max(), 1)  # add assertion here
+        self.assertEqual(min_max(), 2)  # add assertion here
 
 
 if __name__ == '__main__':
