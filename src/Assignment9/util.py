3008780da760d7f3f7a595d9341272c633f6b9da
Surya <surya.c@diggibyte.com>
2024-09-23 18:02:47 +0530

    Floor, Ceil and Rint

--- src/Assignment9/util.py
+++ src/Assignment9/util.py
@@ -1,9 +1,10 @@
+#Floor, Ceil and Rint
 
 import numpy as np
 
-def float_format(n):
+def float_format():
 
-    A=np.array(n.split(','),dtype=float)
+    A=np.array(input().split(','),dtype=float)
 
     return np.ceil(A) , np.floor(A) , np.rint(A)
 
